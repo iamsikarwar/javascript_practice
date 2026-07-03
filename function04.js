54d32f01af1ace3345c3faf3ac4e844db0fd4ff9
Sunil Sikarwar <badcodr@gmail.com>
2026-07-03 13:42:08 +0530

    practice loop and functions

---
+++ function04.js
@@ -0,0 +1,16 @@
+//1
+function add(a , b){
+    return a + b;
+};
+console.log(add(2,5));
+
+//2 function to find max number
+
+function findMax(c, d){
+    if(c>d){
+       return c
+    }else{
+        return d
+    }
+}
+console.log(findMax(10 , 2));
---
+++ loop03.js
@@ -0,0 +1,27 @@
+//Loops 
+//1 print to 100
+for( let a = 1; a >= 100; a++){
+    console.log(a);
+};
+ 
+//2 print even numbers till 100
+
+for(let b = 2; b >=100; b +=2){
+    console.log(b);
+};
+
+//3  print odd numbers
+
+for(c = 1; c < 100; c+=2){
+    console.log(c);
+};
+
+//3 multiplication 
+let e = 2
+ for(let i = 1; i<=10; i++){
+console.log(`${e} * ${i} = ${e*i}`);
+ };
+
+ // factorial
+
+ 
